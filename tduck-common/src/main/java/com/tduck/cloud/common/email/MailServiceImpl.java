d98778b2be5f12e940e2a7b54ccfec7daf4c4fcc
wangqing <250543222@qq.com>
2021-04-08 23:14:45 +0800

    发送邮件异步优化

--- tduck-common/src/main/java/com/tduck/cloud/common/email/MailServiceImpl.java
+++ tduck-common/src/main/java/com/tduck/cloud/common/email/MailServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -69,6 +70,7 @@ public class MailServiceImpl implements MailService {
      * @param content
      */
     @Override
+    @Async
     public void sendHtmlMail(String toAddr, String title, String content) {
         // html 邮件对象
         MimeMessage message = mailSender.createMimeMessage();
