e80256c090601545331ce5d187fcebd678cfecc7
wangqing <smalljop@foxmail.com>
2021-05-19 18:24:11 +0800

    分页

---
+++ tduck-project/src/main/java/com/tduck/cloud/project/entity/struct/PaginationExpandStruct.java
@@ -0,0 +1,13 @@
+package com.tduck.cloud.project.entity.struct;
+
+import lombok.Data;
+
+/**
+ * @author : wangqing
+ * @description : 分页组件属性
+ * @create :  2021/05/19 10:29
+ **/
+@Data
+public class PaginationExpandStruct {
+
+}
